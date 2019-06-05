b1bb78b61d91bbbbff6287e298675af015d4d150
AtlasHale <connor.a.h.h@gmail.com>
2019-06-04 17:01:02 -0700

    misread problem, area under curve given n intervals

--- zero_to_two/tais_formula.py
+++ zero_to_two/tais_formula.py
@@ -1,10 +1,14 @@
 n = int(input())
-x1, h1 = map(float, input().split(" "))
-x2, h2 = map(float, input().split(" "))
-x3, h3 = map(float, input().split(" "))
-x1, x2, x3 = map(int, [x1, x2, x3])
+x_val = []
+y_val = []
+for i in range(n):
+    x1, h1 = map(float, input().split(" "))
+    x_val.append(x1)
+    y_val.append(h1)
+
 total = 0
-total+=(h1+h2)/2*(x2-x1)/1000
-total+=(h3+h2)/2*(x3-x2)/1000
+for i in range(len(x_val)-1):
+    total += (y_val[i]+y_val[i+1])/2*(x_val[i+1]-x_val[i])/1000
+
 print(total)
     
